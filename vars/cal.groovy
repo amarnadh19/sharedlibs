cdf9b8a7c3db49ebf659e3f37e6c7489f4493b89
amarnadh19 <59408654+amarnadh19@users.noreply.github.com>
2022-01-05 14:50:00 +0530

    Create cal.groovy

---
+++ vars/cal.groovy
@@ -0,0 +1,11 @@
+def add(x,y) {
+  echo "addition of ${x} and ${y} is ${x+y}"
+}
+
+def mul(x,y) {
+  echo "multiplication of ${x} and ${y} is ${x*y}"
+}
+
+def sub(x,y) {
+  echo "substraction of ${x} and ${y} is ${x-y}"
+}
